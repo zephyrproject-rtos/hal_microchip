e1ccc121a6ce71bdb5e6dd1b9bf7475a5a3d0ab0
Scott Worley <scott.worley@microchip.com>
2024-06-18 13:24:38 -0400

    hal: microchip: mec5: Update MEC5 HAL VBAT APIs
    Add VBAT register access APIs for VBAT powered power fail
    reset status register and VBAT powered monotonic counter.
    Move VBAT backed memory size define from C source to
    chip specs file.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- mec5/drivers/mec_vbat_api.h
+++ mec5/drivers/mec_vbat_api.h
@@ -18,6 +18,36 @@ extern "C"
 {
 #endif
 
+/* VBAT Power-Fail and Status */
+enum mec_vbat_pfrs {
+    MEC_VBAT_PFRS_RSVD_BIT_0 = 0,
+    MEC_VBAT_PFRS_RSVD_BIT_1,
+    MEC_VBAT_PFRS_PCR_SOFT_RESET_POS,
+    MEC_VBAT_PFRS_JTAG_RESET_POS,
+    MEC_VBAT_PFRS_RESET_IN_PIN_POS,
+    MEC_VBAT_PFRS_WDT_RST_POS,
+    MEC_VBAT_PFRS_ARM_SYSRSTREQ_POS,
+    MEC_VBAT_PFRS_VBAT_RST_POS
+};
+
+uint32_t mec_hal_vbat_pfrs(void);
+void mec_hal_vbat_pfrs_clear(uint32_t clrmsk);
+
+/* Lower word of 64-bit monotonic counter is read-only.
+ * A read causes it to increment by 1.
+ */
+uint32_t mec_hal_read_monotonic_counter32(void);
+
+/* Obtain full 64-bit monotonic counter value
+ * Read causes lower 32-bits to increment by 1.
+ */
+uint64_t mec_hal_read_monotonic_counter64(void);
+
+/* Upper word of 64-bit monotonic counter is read/write.
+ * Read/write does not cause it to increment.
+ */
+void mec_hal_set_monotonic_counter_msw(uint32_t msw);
+
 /* Battery backed memory size in bytes */
 uint32_t mec_hal_bbram_size(void);
 /* Battery backed memory base address */
