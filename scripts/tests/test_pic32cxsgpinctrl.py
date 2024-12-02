fa2431a906ffb560160d40739d7cf04169551103
Michael D Sherwood <michael.sherwood@microchip.com>
2024-12-02 14:43:52 -0700

    hal: microchip: pic32cx: Added 80 pin device selection
    The WLCSP 80 pin package has been created for the PIC32CX device family.
    
    Signed-off-by: Michael D Sherwood <michael.sherwood@microchip.com>

--- scripts/tests/test_pic32cxsgpinctrl.py
+++ scripts/tests/test_pic32cxsgpinctrl.py
@@ -11,6 +11,7 @@ def test_main(data, tmp_path):
 
     FILES = (
         "pic32cx1025sg41064-pinctrl.h",
+        "pic32cx1025sg41080-pinctrl.h",
         "pic32cx1025sg41100-pinctrl.h",
         "pic32cx1025sg41128-pinctrl.h",
         "pic32cx1025sg60100-pinctrl.h",
