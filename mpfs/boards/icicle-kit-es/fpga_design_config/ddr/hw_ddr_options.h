c5f3da5d9bcd8d8fb799a359cb3559d6344804f4
Prajna Rajendra Kumar <prajna.rajendrakumar@microchip.com>
2025-01-17 10:25:30 +0000

    hal: microchip: mpfs: update reference design configuration to v2024.09
    This patch bumps the PolarFire SoC Icicle Kit reference design version
    to v2024.09 release
    https://github.com/polarfire-soc/icicle-kit-reference-design/releases/tag/v2024.09
    
    Signed-off-by: Prajna Rajendra Kumar <prajna.rajendrakumar@microchip.com>

--- mpfs/boards/icicle-kit-es/fpga_design_config/ddr/hw_ddr_options.h
+++ mpfs/boards/icicle-kit-es/fpga_design_config/ddr/hw_ddr_options.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2019-2021 Microchip FPGA Embedded Systems Solutions.
+ * Copyright 2019-2025 Microchip FPGA Embedded Systems Solutions.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -55,18 +55,343 @@ These burn static power.(0x01 => turn off ; 0x00 => no action ) */
 #endif
 #if !defined (LIBERO_SETTING_TIP_CFG_PARAMS)
 /*Tip config: default: 0x2,0x4,0x0,0x1F,0x1F */
-#define LIBERO_SETTING_TIP_CFG_PARAMS    0x07CFE02AUL
-    /* ADDCMD_OFFSET                     [0:3]   RW value= 0x2 */
+#define LIBERO_SETTING_TIP_CFG_PARAMS    0x07CFE02FUL
+    /* ADDCMD_OFFSET                     [0:3]   RW value= 0x7 */
     /* BCKLSCLK_OFFSET                   [3:3]   RW value= 0x5 */
     /* WRCALIB_WRITE_COUNT               [6:7]   RW value= 0x0 */
-    /* READ_GATE_MIN_READS               [13:8]  RW value= 0x7F */
-    /* ADDRCMD_WAIT_COUNT                [22:8]  RW value= 0x1F */
+    /* READ_GATE_MIN_READS               [13:9]  RW value= 0x7F */
+    /* ADDRCMD_WAIT_COUNT                [22:9]  RW value= 0x1F */
 #endif
 #if !defined (LIBERO_SETTING_TIP_CONFIG_PARAMS_BCLK_VCOPHS_OFFSET)
 /*in simulation we need to set this to 2, for hardware it will be dependent on
 the trace lengths */
 #define LIBERO_SETTING_TIP_CONFIG_PARAMS_BCLK_VCOPHS_OFFSET    0x00000002UL
     /* TIP_CONFIG_PARAMS_BCLK_VCOPHS     [0:32]  RW value= 0x02 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1333_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_DDR3_1333_NUM_OFFSETS    0x00000003UL
+    /* REFCLK_DDR3_1333_NUM_OFFSETS      [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1333_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_DDR3L_1333_NUM_OFFSETS    0x00000003UL
+    /* REFCLK_DDR3L_1333_NUM_OFFSETS     [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1600_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_DDR4_1600_NUM_OFFSETS    0x00000004UL
+    /* REFCLK_DDR4_1600_NUM_OFFSETS      [0:32]  RW value= 4 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1600_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1600_NUM_OFFSETS    0x00000003UL
+    /* REFCLK_LPDDR3_1600_NUM_OFFSETS    [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1600_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1600_NUM_OFFSETS    0x00000001UL
+    /* REFCLK_LPDDR4_1600_NUM_OFFSETS    [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1067_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_DDR3_1067_NUM_OFFSETS    0x00000001UL
+    /* REFCLK_DDR3_1067_NUM_OFFSETS      [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1067_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_DDR3L_1067_NUM_OFFSETS    0x00000001UL
+    /* REFCLK_DDR3L_1067_NUM_OFFSETS     [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1333_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_DDR4_1333_NUM_OFFSETS    0x00000004UL
+    /* REFCLK_DDR4_1333_NUM_OFFSETS      [0:32]  RW value= 4 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1333_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1333_NUM_OFFSETS    0x00000002UL
+    /* REFCLK_LPDDR3_1333_NUM_OFFSETS    [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1333_NUM_OFFSETS)
+/*Number of offsets used when training */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1333_NUM_OFFSETS    0x00000003UL
+    /* REFCLK_LPDDR4_1333_NUM_OFFSETS    [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_0    0x00000000UL
+    /* REFCLK_DDR3_1333_OFFSET_0         [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_1    0x00000001UL
+    /* REFCLK_DDR3_1333_OFFSET_1         [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_2    0x00000007UL
+    /* REFCLK_DDR3_1333_OFFSET_2         [0:32]  RW value= 7 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_DDR3_1333_OFFSET_3    0x00000000UL
+    /* REFCLK_DDR3_1333_OFFSET_3         [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_0    0x00000000UL
+    /* REFCLK_DDR3L_1333_OFFSET_0        [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_1    0x00000001UL
+    /* REFCLK_DDR3L_1333_OFFSET_1        [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_2    0x00000000UL
+    /* REFCLK_DDR3L_1333_OFFSET_2        [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1333_OFFSET_3    0x00000000UL
+    /* REFCLK_DDR3L_1333_OFFSET_3        [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_0    0x00000007UL
+    /* REFCLK_DDR4_1600_OFFSET_0         [0:32]  RW value= 7 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_1    0x00000006UL
+    /* REFCLK_DDR4_1600_OFFSET_1         [0:32]  RW value= 6 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_2    0x00000005UL
+    /* REFCLK_DDR4_1600_OFFSET_2         [0:32]  RW value= 5 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_DDR4_1600_OFFSET_3    0x00000000UL
+    /* REFCLK_DDR4_1600_OFFSET_3         [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_0    0x00000007UL
+    /* REFCLK_LPDDR3_1600_OFFSET_0       [0:32]  RW value= 7 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_1    0x00000000UL
+    /* REFCLK_LPDDR3_1600_OFFSET_1       [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_2    0x00000001UL
+    /* REFCLK_LPDDR3_1600_OFFSET_2       [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1600_OFFSET_3    0x00000002UL
+    /* REFCLK_LPDDR3_1600_OFFSET_3       [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_0    0x00000003UL
+    /* REFCLK_LPDDR4_1600_OFFSET_0       [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_1    0x00000004UL
+    /* REFCLK_LPDDR4_1600_OFFSET_1       [0:32]  RW value= 4 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_2    0x00000002UL
+    /* REFCLK_LPDDR4_1600_OFFSET_2       [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1600_OFFSET_3    0x00000005UL
+    /* REFCLK_LPDDR4_1600_OFFSET_3       [0:32]  RW value= 5 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_0    0x00000001UL
+    /* REFCLK_DDR3_1067_OFFSET_0         [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_1    0x00000002UL
+    /* REFCLK_DDR3_1067_OFFSET_1         [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_2    0x00000003UL
+    /* REFCLK_DDR3_1067_OFFSET_2         [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_DDR3_1067_OFFSET_3    0x00000002UL
+    /* REFCLK_DDR3_1067_OFFSET_3         [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_0    0x00000001UL
+    /* REFCLK_DDR3L_1067_OFFSET_0        [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_1    0x00000002UL
+    /* REFCLK_DDR3L_1067_OFFSET_1        [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_2    0x00000003UL
+    /* REFCLK_DDR3L_1067_OFFSET_2        [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_DDR3L_1067_OFFSET_3    0x00000002UL
+    /* REFCLK_DDR3L_1067_OFFSET_3        [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_0    0x00000000UL
+    /* REFCLK_DDR4_1333_OFFSET_0         [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_1    0x00000001UL
+    /* REFCLK_DDR4_1333_OFFSET_1         [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_2    0x00000006UL
+    /* REFCLK_DDR4_1333_OFFSET_2         [0:32]  RW value= 6 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_DDR4_1333_OFFSET_3    0x00000007UL
+    /* REFCLK_DDR4_1333_OFFSET_3         [0:32]  RW value= 7 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_0    0x00000000UL
+    /* REFCLK_LPDDR3_1333_OFFSET_0       [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_1    0x00000001UL
+    /* REFCLK_LPDDR3_1333_OFFSET_1       [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_2    0x00000006UL
+    /* REFCLK_LPDDR3_1333_OFFSET_2       [0:32]  RW value= 6 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_LPDDR3_1333_OFFSET_3    0x00000000UL
+    /* REFCLK_LPDDR3_1333_OFFSET_3       [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_0)
+/*offset for iteration 0 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_0    0x00000002UL
+    /* REFCLK_LPDDR4_1333_OFFSET_0       [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_1)
+/*offset for iteration 1 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_1    0x00000001UL
+    /* REFCLK_LPDDR4_1333_OFFSET_1       [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_2)
+/*offset for iteration 2 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_2    0x00000003UL
+    /* REFCLK_LPDDR4_1333_OFFSET_2       [0:32]  RW value= 3 */
+#endif
+#if !defined (LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_3)
+/*offset for iteration 3 */
+#define LIBERO_SETTING_REFCLK_LPDDR4_1333_OFFSET_3    0x00000000UL
+    /* REFCLK_LPDDR4_1333_OFFSET_3       [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_DDR3)
+/*Zero degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_DDR3    0x00000000UL
+    /* ZERO_DEGRESS_ORDER                [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_DDR3L)
+/*Zero degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_DDR3L    0x00000000UL
+    /* ZERO_DEGRESS_ORDER                [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_DDR4)
+/*Zero degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_DDR4    0x00000000UL
+    /* ZERO_DEGRESS_ORDER                [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_LPDDR3)
+/*Zero degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_LPDDR3    0x00000000UL
+    /* ZERO_DEGRESS_ORDER                [0:32]  RW value= 0 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_LPDDR4)
+/*Zero degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_0_DEG_LPDDR4    0x00000000UL
+    /* ZERO_DEGRESS_ORDER                [0:32]  RW value= 0x0 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_DDR3)
+/*Forty five degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_DDR3    0x00000001UL
+    /* FORTY_FIVE_DEGRESS_ORDER          [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_DDR3L)
+/*Forty five degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_DDR3L    0x00000001UL
+    /* FORTY_FIVE_DEGRESS_ORDER          [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_DDR4)
+/*Forty five degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_DDR4    0x00000001UL
+    /* FORTY_FIVE_DEGRESS_ORDER          [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_LPDDR3)
+/*Forty five degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_LPDDR3    0x00000001UL
+    /* FORTY_FIVE_DEGRESS_ORDER          [0:32]  RW value= 1 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_LPDDR4)
+/*Forty five degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_45_DEG_LPDDR4    0x00000001UL
+    /* FORTY_FIVE_DEGRESS_ORDER          [0:32]  RW value= 0x1 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_DDR3)
+/*Ninty degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_DDR3    0x00000002UL
+    /* NINTY_DEGRESS_ORDER               [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_DDR3L)
+/*Ninty degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_DDR3L    0x00000002UL
+    /* NINTY_DEGRESS_ORDER               [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_DDR4)
+/*Ninty degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_DDR4    0x00000002UL
+    /* NINTY_DEGRESS_ORDER               [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_LPDDR3)
+/*Ninty degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_LPDDR3    0x00000002UL
+    /* NINTY_DEGRESS_ORDER               [0:32]  RW value= 2 */
+#endif
+#if !defined (LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_LPDDR4)
+/*Ninty degres clock move order */
+#define LIBERO_SETTING_ADD_CMD_CLK_MOVE_ORDER_90_DEG_LPDDR4    0x00000002UL
+    /* NINTY_DEGRESS_ORDER               [0:32]  RW value= 0x2 */
 #endif
 
 #ifdef __cplusplus
