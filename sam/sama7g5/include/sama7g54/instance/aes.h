b131ce3507a301e2d43df672aeb6a4c22772504c
Tony Han <tony.han@microchip.com>
2025-02-13 16:38:44 +0800

    hal: microchip: sam: Add include files for SAMA7G5 series
    The files are from the folder "include" in "Microchip SAMA7G5 Series
    Device Support (1.7.232)".
    Download Link:
    https://packs.download.microchip.com/Microchip.SAMA7G5_DFP.1.7.232.atpack
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

---
+++ sam/sama7g5/include/sama7g54/instance/aes.h
@@ -0,0 +1,34 @@
+/*
+ * Instance header file for ATSAMA7G54D4G
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version None */
+#ifndef _SAMA7G5_AES_INSTANCE_
+#define _SAMA7G5_AES_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for AES peripheral ========== */
+#define AES_CLOCK_ID                             (27)       
+#define AES_DMAC_ID_RX                           (2)        
+#define AES_DMAC_ID_TX                           (1)        
+#define AES_INSTANCE_ID                          (27)       /* Advanced Encryption Standard */
+#define AES_INSTANCE_ID_SINT                     (115)      /* Advanced Encryption Standard, Secure INTerrupt */
+
+#endif /* _SAMA7G5_AES_INSTANCE_ */
---
+++ sam/sama7g5/include/sama7g54/instance/dwdt.h
@@ -0,0 +1,32 @@
+/*
+ * Instance header file for ATSAMA7G54D4G
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version None */
+#ifndef _SAMA7G5_DWDT_INSTANCE_
+#define _SAMA7G5_DWDT_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for DWDT peripheral ========== */
+#define DWDT_INSTANCE_ID_NSW                     (1)        /* Dual Watchdog Timer, Non Secure World, interrupt */
+#define DWDT_INSTANCE_ID_NSW_ALARM               (2)        /* Dual Watchdog Timer, Non Secure World Alarm, interrupt */
+#define DWDT_INSTANCE_ID_SW                      (0)        /* Dual Watchdog Timer, Secure World */
+
+#endif /* _SAMA7G5_DWDT_INSTANCE_ */
