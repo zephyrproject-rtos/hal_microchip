1b9980e8edee877590f96cbc2927efa2bba8a145
Arunprasath P <arunprasath.p@microchip.com>
2025-07-15 15:38:54 +0530

    hal: microchip: sam: add SAM_D5X_E5X device family pack files
    Add device family pack files for samd51, same51, same53 and same54 series.
    https://packs.download.microchip.com/Microchip.SAMD51_DFP.3.8.256.atpack
    https://packs.download.microchip.com/Microchip.SAME51_DFP.3.8.253.atpack
    https://packs.download.microchip.com/Microchip.SAME53_DFP.3.9.238.atpack
    https://packs.download.microchip.com/Microchip.SAME54_DFP.3.10.249.atpack
    
    Signed-off-by: Arunprasath P <arunprasath.p@microchip.com>

---
+++ sam/sam_d5x_e5x/same53/include/instance/icm.h
@@ -0,0 +1,30 @@
+/*
+ * Instance header file for ATSAME53N20A
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2023-03-17T09:50:04Z */
+#ifndef _SAME53_ICM_INSTANCE_
+#define _SAME53_ICM_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for ICM peripheral ========== */
+#define ICM_INSTANCE_ID                          (75)
+
+#endif /* _SAME53_ICM_INSTANCE_ */
---
+++ sam/sam_d5x_e5x/same53/include/instance/supc.h
@@ -0,0 +1,32 @@
+/*
+ * Instance header file for ATSAME53N20A
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2023-03-17T09:50:04Z */
+#ifndef _SAME53_SUPC_INSTANCE_
+#define _SAME53_SUPC_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for SUPC peripheral ========== */
+#define SUPC_BOD12_CALIB_MSB                     (5)
+#define SUPC_BOD33_CALIB_MSB                     (5)
+#define SUPC_INSTANCE_ID                         (6)        /* Instance index for SUPC */
+
+#endif /* _SAME53_SUPC_INSTANCE_ */
