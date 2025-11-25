3b93079456dcf71f9aa4f4ab39592fb5bae2c965
Tony Han <tony.han@microchip.com>
2025-11-25 15:41:18 +0800

    hal: microchip: packs: sam: Add include files for SAMA5D2
    The files are from the "atdf" and "include" folders in
    "Microchip SAMA5D2 Series Device Support (1.14.264)".
    Download Link:
    https://packs.download.microchip.com/Microchip.SAMA5D2_DFP.1.14.264.atpack
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

---
+++ packs/sam/sama5d2/sama5d2/include/instance/pwm.h
@@ -0,0 +1,38 @@
+/*
+ * Instance header file for ATSAMA5D28CLD2G
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2025-07-15T11:00:00Z */
+#ifndef _SAMA5D2_PWM_INSTANCE_
+#define _SAMA5D2_PWM_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for PWM peripheral ========== */
+#define PWM_CLOCK_ID                             (38)
+#define PWM_DMAC_ID_TX                           (10)
+#define PWM_FAULT_PWM_ID0                        (0x0)      /* Fault 0 - PWM0_PWMFI0 Input pin */
+#define PWM_FAULT_PWM_ID1                        (0x1)      /* Fault 1 - PWM0_PWMFI1 Input pin */
+#define PWM_FAULT_PWM_ID2                        (0x2)      /* Fault 2 - MAIN_OSC_PMC */
+#define PWM_FAULT_PWM_ID3                        (0x3)      /* Fault 3 - ADC */
+#define PWM_FAULT_PWM_ID4                        (0x4)      /* Fault 4 - TIMER0 */
+#define PWM_FAULT_PWM_ID5                        (0x5)      /* Fault 5 - TIMER1 */
+#define PWM_INSTANCE_ID                          (38)
+
+#endif /* _SAMA5D2_PWM_INSTANCE_ */
---
+++ packs/sam/sama5d2/sama5d27som/include/instance/flexcom1.h
@@ -0,0 +1,36 @@
+/*
+ * Instance header file for ATSAMA5D27WLSOM1
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2025-07-15T11:00:00Z */
+#ifndef _SAMA5D2_FLEXCOM1_INSTANCE_
+#define _SAMA5D2_FLEXCOM1_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for FLEXCOM1 peripheral ========== */
+#define FLEXCOM1_CLOCK_ID                        (20)
+#define FLEXCOM1_DMAC_ID_RX                      (14)
+#define FLEXCOM1_DMAC_ID_TX                      (13)
+#define FLEXCOM1_INSTANCE_ID                     (20)
+#define FLEXCOM1_SPI_FIFO_SIZE                   (32)
+#define FLEXCOM1_TWI_FIFO_SIZE                   (16)
+#define FLEXCOM1_USART_FIFO_SIZE                 (32)
+
+#endif /* _SAMA5D2_FLEXCOM1_INSTANCE_ */
