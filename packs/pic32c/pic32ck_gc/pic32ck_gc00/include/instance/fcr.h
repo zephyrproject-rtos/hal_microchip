ec0bc1c1d6eb4607de8bf5fedcae9ca392835681
Hariharan Arumugam <hariharan.arumugam@microchip.com>
2025-11-28 16:29:37 +0530

    hal: microchip: pic32c: Add PIC32CK_GC device family pack files
    Add device family pack files for PIC32CK-GC00 and PIC32CK-GC01 series.
    
    https://packs.download.microchip.com/Microchip.PIC32CK-GC_DFP.1.2.168.atpack
    
    Signed-off-by: Hariharan Arumugam <hariharan.arumugam@microchip.com>

---
+++ packs/pic32c/pic32ck_gc/pic32ck_gc00/include/instance/fcr.h
@@ -0,0 +1,33 @@
+/*
+ * Instance header file for PIC32CK2051GC00144
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2025-04-07T17:07:16Z */
+#ifndef _PIC32CKGC00_FCR_INSTANCE_
+#define _PIC32CKGC00_FCR_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for FCR peripheral ========== */
+#define FCR_INSTANCE_ID                          (1)        /* Instance index for FCR */
+#define FCR_MCLK_ID_AHB                          (4)        /* Index for FCR AHB clock */
+#define FCR_MCLK_ID_APB                          (33)       /* Index for FCR APB clock */
+#define FCR_PAC_ID                               (1)        /* Index for FCR registers write protection */
+
+#endif /* _PIC32CKGC00_FCR_INSTANCE_ */
---
+++ packs/pic32c/pic32ck_gc/pic32ck_gc00/include/instance/mclk.h
@@ -0,0 +1,33 @@
+/*
+ * Instance header file for PIC32CK2051GC00144
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2025-04-07T17:07:16Z */
+#ifndef _PIC32CKGC00_MCLK_INSTANCE_
+#define _PIC32CKGC00_MCLK_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for MCLK peripheral ========== */
+#define MCLK_DIV_CLK_NUM                         (1)        
+#define MCLK_INSTANCE_ID                         (9)        /* Instance index for MCLK */
+#define MCLK_ID_APB                              (41)       /* Index for MCLK APB clock */
+#define MCLK_PAC_ID                              (9)        /* Index for MCLK registers write protection */
+
+#endif /* _PIC32CKGC00_MCLK_INSTANCE_ */
