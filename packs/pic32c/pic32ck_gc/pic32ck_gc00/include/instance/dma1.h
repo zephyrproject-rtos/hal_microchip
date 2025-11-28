6dcf4fa8c850123ab431d0d5b7ebdc5df4e3c1ea
Hariharan Arumugam <hariharan.arumugam@microchip.com>
2025-11-28 16:29:37 +0530

    hal: microchip: pic32c: Add PIC32CK_GC device family pack files
    Add device family pack files for PIC32CK-GC00 and PIC32CK-GC01 series.
    
    https://packs.download.microchip.com/Microchip.PIC32CK-GC_DFP.1.2.168.atpack
    
    Signed-off-by: Hariharan Arumugam <hariharan.arumugam@microchip.com>

---
+++ packs/pic32c/pic32ck_gc/pic32ck_gc00/include/instance/dma1.h
@@ -0,0 +1,35 @@
+/*
+ * Instance header file for PIC32CK2051GC00144
+ *
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2025-04-07T17:07:16Z */
+#ifndef _PIC32CKGC00_DMA1_INSTANCE_
+#define _PIC32CKGC00_DMA1_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for DMA1 peripheral ========== */
+#define DMA1_DMAC_ID_EVSYS                       (1)        /* Events trigger index */
+#define DMA1_DMAC_ID_SWTRG                       (0)        /* Software trigger index */
+#define DMA1_INSTANCE_ID                         (34)       /* Instance index for DMA1 */
+#define DMA1_MCLK_ID_AHB                         (9)        /* Index for DMA1 AHB clock */
+#define DMA1_MCLK_ID_APB                         (66)       /* Index for DMA1 APB clock */
+#define DMA1_PAC_ID                              (34)       /* Index for DMA1 registers write protection */
+
+#endif /* _PIC32CKGC00_DMA1_INSTANCE_ */
