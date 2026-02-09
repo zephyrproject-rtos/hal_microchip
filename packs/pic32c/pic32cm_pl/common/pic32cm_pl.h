20749cbe7caf496e278bdb4476733be8abfa902b
Fabin V Martin <Fabinv.Martin@microchip.com>
2026-02-09 09:34:44 +0530

    hal: microchip: pic32cm_pl: Add mapping file for sercom
    Added mapping files to declare common macro name definitions
    for pic32cm_pl to be used in sercom uart g1 driver.
    
    Signed-off-by: Fabin V Martin <Fabinv.Martin@microchip.com>

---
+++ packs/pic32c/pic32cm_pl/common/pic32cm_pl.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file pic32cm_pl.h
+ * @brief Top-level include for PIC32CM_PL family macro mapping.
+ *
+ * This header provides a central place to include macro mapping headers
+ * for the Microchip PIC32CM_PL family. Use it to include headers that map
+ * device-specific register and bitfield names to generic names for any
+ * required peripheral.
+ *
+ */
+
+#ifndef MICROCHIP_PIC32CM_PL_H_
+#define MICROCHIP_PIC32CM_PL_H_
+
+#include "sercom_pic32cm_pl.h"
+
+#endif /* MICROCHIP_PIC32CM_PL_H_ */
