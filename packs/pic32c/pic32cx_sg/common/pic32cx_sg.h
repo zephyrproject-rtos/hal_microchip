b4f2b220b39c219eb13015a8cdf5af462e783c07
Fabin V Martin <Fabinv.Martin@microchip.com>
2026-02-11 12:09:20 +0530

    hal: microchip: pic32cx_sg: Add mapping file for sercom
    Added mapping files to declare common macro name definitions
    for pic32cx_sg to be used in sercom uart g1 driver.
    
    Signed-off-by: Fabin V Martin <Fabinv.Martin@microchip.com>

---
+++ packs/pic32c/pic32cx_sg/common/pic32cx_sg.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file pic32cx_sg.h
+ * @brief Top-level include for PIC32CX_SG family macro mapping.
+ *
+ * This header provides a central place to include macro mapping headers
+ * for the Microchip PIC32CX_SG family. Use it to include headers that map
+ * device-specific register and bitfield names to generic names for any
+ * required peripheral.
+ *
+ */
+
+#ifndef MICROCHIP_PIC32CX_SG_H_
+#define MICROCHIP_PIC32CX_SG_H_
+
+#include "sercom_pic32cx_sg.h"
+
+#endif /* MICROCHIP_PIC32CX_SG_H_ */
