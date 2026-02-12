5de70965497f00d5db7a4b78e111f9c58411d090
Fabin V Martin <Fabinv.Martin@microchip.com>
2026-02-12 09:51:37 +0530

    hal: microchip: pic32cz_ca: Add mapping file for sercom
    Added mapping files to declare common macro name definitions
    for pic32cz_ca to be used in sercom uart g1 driver.
    
    Signed-off-by: Fabin V Martin <Fabinv.Martin@microchip.com>

---
+++ packs/pic32c/pic32cz_ca/common/pic32cz_ca.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef MICROCHIP_PIC32CZ_CA_H_
+#define MICROCHIP_PIC32CZ_CA_H_
+
+#include "sercom_pic32cz_ca.h"
+
+#endif /* MICROCHIP_PIC32CZ_CA_H_ */
