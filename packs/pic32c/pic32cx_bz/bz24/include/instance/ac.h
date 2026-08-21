787e2f162286138e61a39a08c3e8a93b9fcce122
Ince Tsao <Ince.Tsao@microchip.com>
2026-08-21 10:58:51 +0800

    hal: microchip: pic32c: Add PIC32CX BZ2 device family pack files
    Add device family pack files for PIC32CX-BZ24 Series
    Add device family pack files for PIC32CX-BZ25 Series
    https://packs.download.microchip.com/Microchip.PIC32CX-BZ_DFP.1.4.243.atpack
    
    Signed-off-by: Ince Tsao <Ince.Tsao@microchip.com>

---
+++ packs/pic32c/pic32cx_bz/bz24/include/instance/ac.h
@@ -0,0 +1,36 @@
+/*
+ * Instance header file for PIC32CX1012BZ24032
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2023-10-17T09:04:46Z */
+#ifndef _BZ24_AC_INSTANCE_
+#define _BZ24_AC_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for AC peripheral ========== */
+#define AC_CMP_CHID_PRESENT                      (1)        
+#define AC_COMPCTRL_MUXNEG_OPAMP                 (7)        
+#define AC_GCLK_ID                               (22)       
+#define AC_IMPLEMENTS_VDBLR                      (0)        
+#define AC_INSTANCE_ID                           (69)       
+#define AC_LOAD_CALIB                            (0)        
+#define AC_NUM_CMP                               (1)        
+#define AC_PAIRS                                 (1)        
+#define AC_SPEED_LEVELS                          (2)        
+
+#endif /* _BZ24_AC_INSTANCE_ */
