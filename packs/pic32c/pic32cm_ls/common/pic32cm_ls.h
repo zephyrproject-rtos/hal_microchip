2d31cd1c74e5fba90eb65e0bb5a4969a7e77e899
Sujan D <sujanduraisujan@gmail.com>
2026-09-07 10:34:47 +0530

    pic32c: add pic32cm_ls common headers, dt-bindings, and update CMakeLists

---
+++ packs/pic32c/pic32cm_ls/common/pic32cm_ls.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef MICROCHIP_PIC32CM_LS_H_
+#define MICROCHIP_PIC32CM_LS_H_
+
+#include "sercom_pic32cm_ls.h"
+
+#endif /* MICROCHIP_PIC32CM_LS_H_ */
